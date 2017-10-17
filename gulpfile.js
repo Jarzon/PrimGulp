4eee96426c46fe7d80fd4d8ac8aa3733527d753b
Jarzon <j@masterj.net>
2017-10-17 19:22:17 -0400

    Fix timing bugs by using good ol' function

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,74 @@ gulp.task('msg-clean', function () {
     return del(['app/config/messages.json']);
 });
 
-gulp.task('assets-reload', function(cb) {
+function jsBuild() {
+    let tasks = Object.keys(config.js.files).map(function(key) {
+        return gulp.src(config.js.files[key])
+            .pipe(concat(key))
+            .pipe(gulp.dest(config.js.destination));
+    });
+
+    return es.concat.apply(null, tasks);
+}
+
+function cssBuild() {
+    let tasks = Object.keys(config.css.files).map(function(key) {
+        return gulp.src(config.css.files[key])
+            .pipe(concat(key))
+            .pipe(gulp.dest(config.css.destination));
+    });
+
+    return es.concat.apply(null, tasks);
+}
+
+function imgBuild() {
+    let tasks = Object.keys(config.img.files).map(function(key) {
+        return gulp.src(config.img.files[key])
+            .pipe(rename(function (path) {
+                path.dirname = key;
+            }))
+            .pipe(gulp.dest(config.img.destination));
+    });
+
+    return es.concat.apply(null, tasks);
+}
+
+function filesBuild() {
+    let tasks = Object.keys(config.files.files).map(function(key) {
+        return gulp.src(config.files.files[key])
+            .pipe(rename(function (path) {
+                path.dirname = key;
+            }))
+            .pipe(gulp.dest(config.files.destination));
+    });
+
+    return es.concat.apply(null, tasks);
+}
+
+function msgBuild(cb) {
+    return gulp.src(['src/*/config/messages.json', 'vendor/*/config/messages.json'])
+        .pipe(
+            merge({fileName: 'messages.json', jsonSpace: ''})
+                .on('error', function(err) {
+                    cb(err);
+                })
+        )
+        .pipe(gulp.dest('app/config/'));
+}
+
+function assetsBuild(cb) {
+    return gulp.src(['src/*/config/assets.json', 'vendor/*/config/assets.json'])
+        .pipe(
+            merge({fileName: 'assets.json', jsonSpace: '', concatArrays: true})
+                .on('error', function(err) {
+                    cb(err);
+                })
+        )
+        .pipe(gulp.dest('app/config/'))
+        .on('end', cb);
+}
+
+function assetsReload(cb) {
     fs.readFile('app/config/assets.json', 'utf8', function (err, data) {
         if (err) cb(err);
 
@@ -40,84 +107,62 @@ gulp.task('assets-reload', function(cb) {
 
         cb();
     });
-});
+}
 
-gulp.task('js-build', ['js-clean'], function() {
-    let tasks = Object.keys(config.js.files).map(function(key) {
-        return gulp.src(config.js.files[key])
-            .pipe(concat(key))
-            .pipe(gulp.dest(config.js.destination));
-    });
-
-    return es.concat.apply(null, tasks);
-});
-
-gulp.task('css-build', ['css-clean'], function() {
-    let tasks = Object.keys(config.css.files).map(function(key) {
-        return gulp.src(config.css.files[key])
-            .pipe(concat(key))
-            .pipe(gulp.dest(config.css.destination));
-    });
-
-    return es.concat.apply(null, tasks);
-});
-
-gulp.task('img-build', ['img-clean'], function() {
-    let tasks = Object.keys(config.img.files).map(function(key) {
-        return gulp.src(config.img.files[key])
-            .pipe(rename(function (path) {
-                path.dirname = key;
-            }))
-            .pipe(gulp.dest(config.img.destination));
-    });
-
-    return es.concat.apply(null, tasks);
-});
-
-gulp.task('files-build', ['files-clean'], function() {
-    let tasks = Object.keys(config.files.files).map(function(key) {
-        return gulp.src(config.files.files[key])
-            .pipe(rename(function (path) {
-                path.dirname = key;
-            }))
-            .pipe(gulp.dest(config.files.destination));
-    });
-
-    return es.concat.apply(null, tasks);
-});
-
-gulp.task('msg-build', function(cb) {
-    return gulp.src(['src/*/config/messages.json', 'vendor/*/config/messages.json'])
-        .pipe(
-            merge({fileName: 'messages.json', jsonSpace: ''})
-                .on('error', function(err) {
-                    cb(err);
-                })
-        )
-        .pipe(gulp.dest('app/config/'));
-});
-
-gulp.task('assets-build', function(cb) {
-    return gulp.src(['src/*/config/assets.json', 'vendor/*/config/assets.json'])
-        .pipe(
-            merge({fileName: 'assets.json', jsonSpace: '', concatArrays: true})
-                .on('error', function(err) {
-                    cb(err);
-                })
-        )
-        .pipe(gulp.dest('app/config/'));
-});
-
-gulp.task('watch', function() {
-    gulp.watch(['app/config/assets.json'], ['assets-reload']);
+function watch() {
     gulp.watch(['src/*/assets/js/*.js', 'vendor/*/assets/js/*.js'], ['js-build']);
     gulp.watch(['src/*/assets/css/*.css', 'vendor/*/assets/css/*.css'], ['css-build']);
     gulp.watch(['src/*/assets/img/*', 'vendor/*/assets/img/*'], ['img-build']);
     gulp.watch(['src/*/assets/files/*', 'vendor/*/assets/files/*'], ['files-build']);
     gulp.watch(['src/*/config/messages.json', 'vendor/*/config/messages.json'], ['msg-clean', 'msg-build']);
     gulp.watch(['src/*/config/assets.json', 'vendor/*/config/assets.json'], ['assets-build']);
+    gulp.watch(['app/config/assets.json'], ['assets-reload']);
+}
+
+gulp.task('js-build', ['js-clean'], function() {
+    return jsBuild();
 });
 
-gulp.task('default', ['watch', 'assets-build'], function(){});
+gulp.task('css-build', ['css-clean'], function() {
+    return cssBuild();
+});
+
+gulp.task('img-build', ['img-clean'], function() {
+    return imgBuild();
+});
+
+gulp.task('files-build', ['files-clean'], function() {
+    return filesBuild();
+});
+
+gulp.task('msg-build', function(cb) {
+    return msgBuild(cb);
+});
+
+gulp.task('assets-build', function(cb) {
+    return assetsBuild(cb);
+});
+
+gulp.task('assets-reload', function(cb) {
+    assetsReload(cb);
+});
+
+gulp.task('watch', function() {
+    watch();
+});
+
+gulp.task('default', [], function() {
+    assetsBuild(function() {
+        assetsReload(function() {
+            msgBuild({});
+            filesBuild();
+            imgBuild();
+            cssBuild();
+            jsBuild();
+        });
+    });
+
+    watch();
+});
 
 module.exports = gulp;
